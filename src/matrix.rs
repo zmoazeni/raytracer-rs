9308a71ca1a9fb8fbd088adb7e128c092114f399
Zach Moazeni <zach.moazeni@gmail.com>
2019-04-25 19:38:12 -0400

    Some tests around indexing

--- src/matrix.rs
+++ src/matrix.rs
@@ -18,6 +18,24 @@ impl Matrix {
         Matrix { dimensions: (width, height), values: v2 }
     }
 
+    pub fn with_values(values: Vec<Vec<f32>>) -> Matrix {
+        // let mut m = Self::new(height, width);
+        let height = values.len();
+        let width = values.first().expect("matrix requires at least one row").len();
+        let mut m = Self::new(height, width);
+
+        for (y, row) in values.iter().enumerate() {
+            if row.len() != width {
+                panic!("matrix is not uniform {}x{}. Row {} has {} column((s).", height, width, y, row.len());
+            }
+
+            for (x, value) in row.iter().enumerate() {
+                m[(y, x)] = *value;
+            }
+        }
+        m
+    }
+
     pub fn iter(&self) -> DimensionalIterator {
         DimensionalIterator::matrix(self.dimensions)
     }
@@ -54,5 +72,49 @@ mod test {
             assert_eq!(value, 0.0);
         }
         assert_eq!(count, 12);
+    }
+
+    #[test]
+    fn indexing1() {
+        let m = Matrix::with_values(vec![
+            vec![1.0, 2.0, 3.0, 4.0],
+            vec![5.5, 6.5, 7.5, 8.5],
+            vec![9.0, 10.0, 11.0, 12.0],
+            vec![13.5, 14.5, 15.5, 16.5],
+        ]);
+
+        assert_eq!(m[(0,0)], 1.0);
+        assert_eq!(m[(0,3)], 4.0);
+        assert_eq!(m[(1,0)], 5.5);
+        assert_eq!(m[(1,2)], 7.5);
+        assert_eq!(m[(2,2)], 11.0);
+        assert_eq!(m[(3,0)], 13.5);
+        assert_eq!(m[(3,2)], 15.5);
+    }
+
+    #[test]
+    fn indexing2() {
+        let m = Matrix::with_values(vec![
+            vec![-3.0, 5.0],
+            vec![1.0, -2.0],
+        ]);
+
+        assert_eq!(m[(0,0)], -3.0);
+        assert_eq!(m[(0,1)], 5.0);
+        assert_eq!(m[(1,0)], 1.0);
+        assert_eq!(m[(1,1)], -2.0);
+    }
+
+    #[test]
+    fn indexing3() {
+        let m = Matrix::with_values(vec![
+            vec![-3.0, 5.0, 0.0],
+            vec![1.0, -2.0, -7.0],
+            vec![0.0, 1.0, 1.0],
+        ]);
+
+        assert_eq!(m[(0,0)], -3.0);
+        assert_eq!(m[(1,1)], -2.0);
+        assert_eq!(m[(2,2)], 1.0);
     }
 }
