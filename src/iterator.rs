aeb57c256e3a0e1f0c012eaa7040942f180b44f4
Zach Moazeni <zach.moazeni@gmail.com>
2019-04-26 22:50:49 -0400

    Multiplication and fixed iterating matrixes

--- src/iterator.rs
+++ src/iterator.rs
@@ -17,7 +17,7 @@ impl DimensionalIterator {
 
     pub fn matrix(dimensions: (usize, usize)) -> DimensionalIterator {
         DimensionalIterator {
-            dimensions: (dimensions.1, dimensions.0),
+            dimensions: dimensions,
             position: (0, 0),
             first_pass: true,
             dimensions_reversed: true,
@@ -37,6 +37,7 @@ impl Iterator for DimensionalIterator {
     type Item = (usize, usize);
 
     fn next(&mut self) -> Option<(usize, usize)> {
+
         let (width, height) = reverse_tuple(self.dimensions, self.dimensions_reversed);
 
         let (x, y) = self.position;
@@ -51,9 +52,42 @@ impl Iterator for DimensionalIterator {
         } else if y + 1 < height {
             self.position = (0, y + 1);
         } else {
-            return None;
+            return None
         }
 
         return Some(reverse_tuple(self.position, self.dimensions_reversed))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn iterate_canvas() {
+        let iter = DimensionalIterator::canvas((4, 2));
+        let mut v = vec![];
+        for coordinate in iter {
+            v.push(coordinate);
+        }
+        assert_eq!(v, vec![
+            (0, 0), (1, 0), (2, 0), (3, 0),
+            (0, 1), (1, 1), (2, 1), (3, 1)
+        ]);
+    }
+
+    #[test]
+    fn iterate_matrix() {
+        let iter = DimensionalIterator::matrix((4, 2));
+        let mut v = vec![];
+        for coordinate in iter {
+            v.push(coordinate);
+        }
+        assert_eq!(v, vec![
+            (0, 0), (0, 1),
+            (1, 0), (1, 1),
+            (2, 0), (2, 1),
+            (3, 0), (3, 1),
+        ]);
     }
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,6 +1,6 @@
 use super::iterator::*;
 use super::util::*;
-use std::ops::{Index,IndexMut};
+use std::ops::{Index,IndexMut,Mul};
 use std::cmp::{PartialEq,Eq};
 
 #[allow(unused_macros)]
@@ -18,10 +18,20 @@ macro_rules! matrix {
     )
 }
 
+#[allow(unused_macros)]
+macro_rules! tuple {
+    ( $($x:expr),* ) => {
+        matrix![
+            $($x);*
+        ]
+    };
+}
+
 
 #[derive(Debug,Clone)]
 pub struct Matrix {
-    pub dimensions: (usize, usize),
+    height: usize,
+    width: usize,
     values: Vec<Vec<f32>>,
 }
 
@@ -33,7 +43,7 @@ impl Matrix {
             v2.push(row.clone());
         }
 
-        Matrix { dimensions: (width, height), values: v2 }
+        Matrix { height, width, values: v2 }
     }
 
     pub fn with_values(values: Vec<Vec<f32>>) -> Matrix {
@@ -54,7 +64,11 @@ impl Matrix {
     }
 
     pub fn iter(&self) -> DimensionalIterator {
-        DimensionalIterator::matrix(self.dimensions)
+        DimensionalIterator::matrix(self.dimensions())
+    }
+
+    pub fn dimensions(&self) -> (usize, usize) {
+        (self.height, self.width)
     }
 }
 
@@ -73,7 +87,7 @@ impl IndexMut<(usize, usize)> for Matrix {
 
 impl PartialEq for Matrix {
     fn eq(&self, rhs: &Matrix) -> bool {
-        if self.dimensions != rhs.dimensions {
+        if self.dimensions() != rhs.dimensions() {
             return false;
         }
         self.iter().all(|dimensions| {
@@ -83,6 +97,25 @@ impl PartialEq for Matrix {
 }
 impl Eq for Matrix {}
 
+impl Mul<Matrix> for Matrix {
+    type Output = Option<Self>;
+    fn mul(self, rhs: Self) -> Option<Self> {
+        if self.width != rhs.height {
+            return None;
+        }
+
+        let mut result = Matrix::new(self.height, rhs.width);
+        for (y, x) in result.iter() {
+            let mut sum = 0.0;
+            for i in 0..self.width {
+                sum += self[(y, i)] * rhs[(i, x)];
+            }
+            result[(y, x)] = sum;
+        }
+        Some(result)
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -90,9 +123,8 @@ mod test {
     #[test]
     fn default_matrix() {
         let m = Matrix::new(4, 3);
-        let (width, height) = m.dimensions;
-        assert_eq!(height, 4);
-        assert_eq!(width, 3);
+        assert_eq!(m.height, 4);
+        assert_eq!(m.width, 3);
 
         let mut count = 0;
         for (y, x) in m.iter() {
@@ -172,5 +204,64 @@ mod test {
             3.0, 10.0;
         ];
         assert_ne!(m, m4);
+    }
+
+    #[test]
+    fn multiplication4x4() {
+        let m1 = matrix![
+            1.0, 2.0, 3.0, 4.0;
+            5.0, 6.0, 7.0, 8.0;
+            9.0, 8.0, 7.0, 6.0;
+            5.0, 4.0, 3.0, 2.0
+        ];
+        let m2 = matrix![
+            -2.0, 1.0, 2.0, 3.0;
+            3.0, 2.0, 1.0, -1.0;
+            4.0, 3.0, 6.0, 5.0;
+            1.0, 2.0, 7.0, 8.0
+        ];
+        let expected = matrix![
+            20.0, 22.0, 50.0, 48.0;
+            44.0, 54.0, 114.0, 108.0;
+            40.0, 58.0, 110.0, 102.0;
+            16.0, 26.0, 46.0, 42.0
+        ];
+        assert_eq!(Some(expected), m1 * m2);
+    }
+
+    #[test]
+    fn multiplication_wrong_sizes() {
+        let m1 = matrix![
+            1.0, 2.0;
+            3.0, 4.0
+        ];
+        let m2 = matrix![
+            1.0, 2.0, 3.0;
+        ];
+        println!("{:?} {:?}", m1, m2);
+        assert_eq!(None, m1 * m2);
+    }
+
+    #[test]
+    fn multiplication_tuple() {
+        let m1 = matrix![
+            1.0, 2.0, 3.0, 4.0;
+            2.0, 4.0, 4.0, 2.0;
+            8.0, 6.0, 4.0, 1.0;
+            0.0, 0.0, 0.0, 1.0
+        ];
+        let tuple = tuple![
+            1.0, 2.0, 3.0, 1.0
+        ];
+        assert_eq!(4, tuple.height);
+        assert_eq!(1, tuple.width);
+
+        let expected = matrix![
+            18.0;
+            24.0;
+            33.0;
+            1.0
+        ];
+        assert_eq!(Some(expected), m1 * tuple);
     }
 }
